36af18d9f7b539843f6db61a8ca267e1f629ac2a
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 03:11:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,16 @@ function logWhisper(text) {
 }
 
 function sayHiToGrandma(text) {
-  
+  if (text.toLowerCase() === text)
+  {
+    return "I can't hear you!";
+  }
+  else if (text.toUpperCase() === text)
+  {
+    return "YES, INDEED!";
+  }
+  else if (text === "I love you, Grandma")
+  {
+    return "I love you, too";
+  }
 }
